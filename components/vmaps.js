bb96da10a75e78958953d5aff20383893b826eb1
Abraham S <abrahamsalloum@gmail.com>
2021-09-14 16:13:16 +1000

    fixes

--- components/vmaps.js
+++ components/vmaps.js
@@ -2,45 +2,46 @@
 
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent, useMapEvents, MapConsumer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, memo } from 'react'
 
-const  Vmap = ({latlong, parentCallback}) => {
+const  Vmap = ({parentCallback}) => {
 
-    const [maplatlong, setlonglat] = useState([false,false])
+  const [latlong, setLatLong] = useState([51.505,-0.09])
 
-   useEffect(() => {
-    setlonglat(latlong)
-   },[])   
-  
-   while(maplatlong[0] === false) return '..'
-  
-  const SetCenter = () => {
-
+  const  InitialThings = () => {
     const map = useMapEvents({
-      moveend: (ev) => {
-        let ltlng = [ev.target.getCenter().lat, ev.target.getCenter().lng]
-        setlonglat(ltlng)
-        parentCallback(ltlng)
-        return null
+      click(ev) {
+        map.locate()
       },
-
-
-    })
+      load(ev){
+        map.locate()
+      },
+      locationfound(ev) {
+        setLatLong(ev.latlng)
+        map.setView(ev.latlng)
+      },
+      moveend(ev){
+      let ltlng = [ev.target.getCenter().lat, ev.target.getCenter().lng]
+      setLatLong(ltlng)
+      parentCallback(ltlng)
+      },
+    }) 
     return null
   }
 
     return(
         <div style={{ height: "100%", width: "100%", margin: 0, padding: 0 }}>
-        {JSON.stringify(maplatlong)}
-        <MapContainer center={maplatlong} zoom={13} scrollWheelZoom={true} style={{ height: "450px", width: "100%" }}>
+        {JSON.stringify(latlong)}
+        <MapContainer center={latlong} zoom={13} scrollWheelZoom={true} style={{ height: "450px", width: "100%" }}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        <SetCenter />
+        
+        <InitialThings />
       </MapContainer>           
       </div>
-  )
+    )
 }
 
-export default Vmap
+export default memo(Vmap)
