b59baf64a2eb084ce5b87845c517350c4da0f768
root <abrahamsalloum@gmail.com>
2024-11-09 21:21:47 +0000

    refactoring

--- components/mainweather.js
+++ components/mainweather.js
@@ -8,11 +8,15 @@ import CloudCoverGraph from './mainweather/CloudCoverGraph';
 import WindSpeedGraph from './mainweather/WindSpeedGraph';
 import WeeklyForcast from './mainweather/WeeklyForecast';
 import Quicksummary from './mainweather/QuickSummary';
+import styles from '../styles/Home.module.css'
+import SearchBox from '../components/SearchBox'
+import Head from 'next/head'
+import dynamic from 'next/dynamic';
+const VMap = dynamic(() => import('../components/vmaps'), { ssr: false })
 
 
-const MainWeather = () => {
+const MainWeather = ({ latlong }) => {
 
-    const { latlong } = useMapProvider()
     const [weather, setweather] = useState(false)
     const [chartdata, setChartData] = useState(false)
     const [tz_name, setTz_name] = useState('UTC')
@@ -21,10 +25,11 @@ const MainWeather = () => {
         const tz = await fetch(`/api/tz/${latlong.join(',')}`)
         const tz_name_f = await tz.json()
         setTz_name(tz_name_f.tz[0])
+
         const weather = await fetch(`https://api.open-meteo.com/v1/forecast?${createAPISting(tz_name_f)}`)
-        const w = await weather.json()
-        setweather(w)
-        getdata(w)
+        const rawWeatherData = await weather.json()
+        setweather(rawWeatherData)
+        formatData(rawWeatherData)
     }
 
 
@@ -32,8 +37,8 @@ const MainWeather = () => {
         let params = {
             latitude: [latlong[0]],
             longitude: [latlong[0]],
-            hourly: ["temperature_2m","relative_humidity_2m","apparent_temperature","precipitation","cloudcover","windspeed_10m","cloudcover","weathercode","winddirection_10m"],
-            daily: ["weathercode","temperature_2m_max","temperature_2m_min","apparent_temperature_max","apparent_temperature_min","sunrise","sunset","precipitation_sum","precipitation_hours"],
+            hourly: ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "precipitation", "cloudcover", "windspeed_10m", "cloudcover", "weathercode", "winddirection_10m"],
+            daily: ["weathercode", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset", "precipitation_sum", "precipitation_hours"],
             timezone: [tz_name_f.tz[0]]
         }
         let urlstring = "";
@@ -41,24 +46,24 @@ const MainWeather = () => {
             urlstring += `${item}=${params[item].join(',')}&`
         })
 
-        return urlstring; 
+        return urlstring;
     }
 
-    const getdata = (w) => {
+    const formatData = (rawWeatherData) => {
         let graphdata = []
-        for (let i in w.hourly.time) {
+        for (let i in rawWeatherData.hourly.time) {
             graphdata.push({
-                unixtime: DateTime.fromISO(w.hourly.time[i]).toMillis(),
-                rawtime: w.hourly.time[i],
-                name: DateTime.fromISO(w.hourly.time[i], { zone: tz_name }).toLocaleString(DateTime.DATETIME_SHORT),
-                uv: w?.hourly?.temperature_2m?.[i],
-                pv: w?.hourly?.apparent_temperature?.[i],
-                cc: w?.hourly?.cloudcover?.[i],
-                weathercode: w?.hourly?.weathercode?.[i],
-                precipitation: w?.hourly?.precipitation?.[i],
-                windspeed: w?.hourly?.windspeed_10m?.[i],
-                winddirection: w?.hourly?.winddirection_10m?.[i],
-                humidity: w?.hourly?.relative_humidity_2m?.[i]
+                unixtime: DateTime.fromISO(rawWeatherData.hourly.time[i]).toMillis(),
+                rawtime: rawWeatherData.hourly.time[i],
+                name: DateTime.fromISO(rawWeatherData.hourly.time[i], { zone: tz_name }).toLocaleString(DateTime.DATETIME_SHORT),
+                uv: rawWeatherData?.hourly?.temperature_2m?.[i],
+                pv: rawWeatherData?.hourly?.apparent_temperature?.[i],
+                cc: rawWeatherData?.hourly?.cloudcover?.[i],
+                weathercode: rawWeatherData?.hourly?.weathercode?.[i],
+                precipitation: rawWeatherData?.hourly?.precipitation?.[i],
+                windspeed: rawWeatherData?.hourly?.windspeed_10m?.[i],
+                winddirection: rawWeatherData?.hourly?.winddirection_10m?.[i],
+                humidity: rawWeatherData?.hourly?.relative_humidity_2m?.[i]
             })
         }
 
@@ -72,67 +77,33 @@ const MainWeather = () => {
     }
 
     return (
-        <div style={{ "display": "flex", "flexDirection": "column", "width": "100%" }}>
-            <div style={{ "display": "flex", "flexDirection": "row", "width": "100%", "alignItems":"center" }}>
-                <button onClick={() => getweather()} className="bbutton">Get Weather</button>
-                {!!chartdata ? <Quicksummary chartdata={chartdata} /> : null}
-                <style jsx>{`
-                        .bbutton {
-                            background-color: skyblue;
-                            border: none;
-                            padding: 5px;
-                            white-space: nowrap; 
-                            text-align: center;
-                            height: fit-content;
-                            height:50px;
-                        }
-                    `}</style>
+        <>
+            <div style={{ "display": "flex", "flexDirection": "row", "width": "100%", "alignItems": "center" }}>
+                <button onClick={() => getweather()} className={styles.bbutton}>Get Weather</button>
             </div>
+            {!!chartdata ? <WeatherData weather={weather} chartdata={chartdata} tz_name={tz_name} /> : null }
+        </>
+    )
+
+}
+
+
+const WeatherData = ({ weather, chartdata,  tz_name}) => {
+    return (
+        <div style={{ "display": "flex", "flexDirection": "column", "width": "100%" }}>
+
             {!!chartdata ?
                 <>
-                    <div className="graph">
+                    <div className={styles.graph}>
                         <TempHumidityRainGraph chartdata={chartdata} />
                     </div >
-                    <div className="split">
-                        <div className="graph">
+                    <div className={styles.split}>
+                        <div className={styles.graph}>
                             <CloudCoverGraph chartdata={chartdata} />
                         </div>
-                        <div className="graph">
+                        <div className={styles.graph}>
                             <WindSpeedGraph chartdata={chartdata} />
                         </div>
-
-                        <style jsx>{
-                        `
-                        .split {
-                            display: flex; 
-                            flex-direction: row;
-                            width: 100%; 
-                            height: 100%;
-                        }
-
-                        @media (max-width: 1200px) {
-                            .split {
-                                width: 100%; 
-                                height: 100%;
-                                flex-direction: column;
-                            }
-                        }
-
-                        .graph {
-                            width: 100%;
-                            height: 100%;
-                        }
-
-                        .bbutton {
-                            background-color: skyblue;
-                            border: none;
-                            padding: 5px;
-                            white-space: "nowrap; 
-                            text-align: center;
-                            height: fit-content;
-                        }
-                        `}</style>
-
                     </div>
                     <div><WeeklyForcast weather={weather} tz_name={tz_name} /></div>
                 </>
@@ -142,4 +113,31 @@ const MainWeather = () => {
     )
 }
 
-export default MainWeather
+const WeatherWrapper = ({ host, latlong }) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.menu}>
+                <div><a href={`//${host}/${latlong.join(',')}`}>{`/${latlong.join(',')}`}</a></div>
+                <div className={styles.searchWrapper}>
+                    <SearchBox />
+                </div>
+            </div>
+            <Head>
+                <title>Weather</title>
+                <meta name="description" content="Weather" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+                <h2 className={styles.title}>Weather</h2>
+                <div>Click in map for best guess location or search</div>
+                <VMap />
+                <MainWeather latlong={latlong} />
+            </main>
+            <footer className={styles.footer}>
+                Data Provided by<pre> <a href="https://open-meteo.com/" target="_blank" rel="noopener noreferrer"> https://open-meteo.com</a></pre>
+            </footer>
+        </div>
+    );
+}
+
+export default WeatherWrapper
--- pages/[[...loc]].js
+++ pages/[[...loc]].js
@@ -1,11 +1,7 @@
-import Head from 'next/head'
 import { useEffect, useState} from 'react';
-import styles from '../styles/Home.module.css'
 import dynamic from 'next/dynamic';
-import MainWeather from '../components/mainweather'
-import SearchBox from '../components/SearchBox'
 import { useMapProvider } from "../components/MapProvider";
-const VMap = dynamic(() => import('../components/vmaps'),{ ssr: false })
+import WeatherWrapper from '../components/mainweather';
 
 
 export default function Home() {
@@ -18,45 +14,6 @@ export default function Home() {
 
  
   return (
-    <div className={styles.container}>
-      <div className="menu">
-        <div><a href={`//${host}/${latlong.join(',')}`}>{`/${latlong.join(',')}`}</a></div>
-        <div className="searchWrapper"><SearchBox/></div>
-      </div>
-      <Head>
-        <title>Weather</title>
-        <meta name="description" content="Weather" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h2 className={styles.title}>Weather</h2>
-        <div>Click in map for best guess location or search</div>
-        <VMap />
-        <MainWeather />  
-      </main>
-      <footer className={styles.footer}>
-        Data Provided by<pre> <a href="https://open-meteo.com/" target="_blank" rel="noopener noreferrer"> https://open-meteo.com</a></pre>
-      </footer>
-      <style jsx>{`
-        .menu {
-          display: flex; 
-          width: 100%;
-          justify-content: space-between;
-        }
-
-        @media (max-width: 500px) {
-          .menu {
-            flex-direction: column; 
-            justify-content: space-evenly;
-            width: 100%;
-          }
-        }
-        
-        .searchWrapper {
-          height: 0px; 
-          z-index: 999; 
-        }
-      `}</style>
-    </div>
+        <WeatherWrapper host={host} latlong={latlong}/>
   ) 
 }
